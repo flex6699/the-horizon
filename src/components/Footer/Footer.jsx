4306e6e7e675af9b875a6fdd21ff91a73255c4c5
flex6699 <54765869+flex6699@users.noreply.github.com>
2024-06-07 22:55:39 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer id="footer">
       <div className="footer-content">
         <div className="footer-content-left">
-          <img src={assets.logo} alt="logo" />
+          <img src={assets.logo} alt="logo" className="logo" />
           <p>
             Welcome to The Horizon, where culinary excellence meets serene
             ambiance. Indulge in our diverse menu offerings crafted to elevate
@@ -15,7 +15,7 @@ const Footer = () => {
           </p>
         </div>
       </div>
-     
+
       <h2> Phone Number:+91965179201,7217507179</h2>
 
       <h3>Email:thehorizonhospitalitycorp@gmail.com</h3>
