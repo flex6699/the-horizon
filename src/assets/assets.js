0aae231aa096a22e999b56ebd30372306f2be685
flex6699 <54765869+flex6699@users.noreply.github.com>
2024-06-07 22:39:20 +0530

    Add files via upload

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -15,7 +15,70 @@ import img1 from "./img1.jpg";
 import img2 from "./img2.jpg";
 import img3 from "./img3.jpg";
 import img4 from "./img4.jpg";
-import vid1 from "./v.mp4";
+import vid1 from "./v1.mp4";
+import img5 from "./bg1.jpg";
+import img6 from "./bg2.jpg";
+import img7 from "./bg3.jpg";
+import img8 from "./bg4.jpg";
+import img9 from "./bg5.jpg";
+import vid2 from "./v2.mp4";
+
+import f1 from "./f1.png";
+import f2 from "./f2.png";
+import f3 from "./f3.png";
+import f4 from "./f4.png";
+import f5 from "./f5.png";
+import f6 from "./f6.png";
+import f7 from "./f7.png";
+import f8 from "./f8.png";
+import f9 from "./f9.png";
+import f10 from "./f10.png";
+import f11 from "./f11.png";
+import f12 from "./f12.png";
+import f13 from "./f13.png";
+import f14 from "./f14.png";
+import f15 from "./f15.png";
+import f16 from "./f16.png";
+import f17 from "./f17.png";
+import f18 from "./f18.png";
+import f19 from "./f19.png";
+import f20 from "./f20.png";
+import f21 from "./f21.png";
+import f22 from "./f22.png";
+import f23 from "./f23.png";
+import f24 from "./f24.png";
+import f25 from "./f25.png";
+import f26 from "./f26.png";
+import f27 from "./f27.png";
+import f28 from "./f28.png";
+import f29 from "./f29.png";
+import f30 from "./f30.png";
+import f31 from "./f31.png";
+import f32 from "./f32.png";
+import f33 from "./f33.png";
+import f34 from "./f34.png";
+import f35 from "./f35.png";
+import f36 from "./f36.png";
+import f37 from "./f37.png";
+import f38 from "./f38.png";
+import f39 from "./f39.png";
+import f40 from "./f40.png";
+import f41 from "./f41.png";
+import f42 from "./f42.png";
+import f43 from "./f43.png";
+import f44 from "./f44.png";
+import f45 from "./f45.png";
+import f46 from "./f46.png";
+import f47 from "./f47.png";
+import f48 from "./f48.png";
+import f49 from "./f49.png";
+import f50 from "./f50.png";
+import f51 from "./f51.png";
+import f52 from "./f52.png";
+import f53 from "./f53.png";
+import f54 from "./f54.png";
+import f55 from "./f55.png";
+import f56 from "./f56.png";
 
 import add_icon_white from "./add_icon_white.png";
 import add_icon_green from "./add_icon_green.png";
@@ -56,10 +119,16 @@ export const assets = {
   bag_icon,
   parcel_icon,
   img1,
+  vid1,
   img2,
   img3,
   img4,
-  vid1,
+  vid2,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
 };
 
 export const menu_list = [
@@ -68,7 +137,7 @@ export const menu_list = [
     menu_image: menu_1,
   },
   {
-    menu_name: "Tandoor ",
+    menu_name: "Tandoor",
     menu_image: menu_2,
   },
   {
@@ -101,431 +170,468 @@ export const food_list = [
   {
     _id: "1",
     name: "Paneer Kathi Rolls",
-    price: "₹10",
+    price: "₹100",
     description: "Delicious paneer wrapped in a soft roll with spices.",
     category: "Chinese",
-    image: "src/assets/f1.png",
+    image: f1,
   },
   {
     _id: "2",
     name: "Egg Noodles",
-    price: "₹8",
+    price: "₹100",
     description: "Stir-fried noodles with eggs and vegetables.",
     category: "Chinese",
-    image: "src/assets/f2.png",
+    image: f2,
   },
   {
     _id: "3",
     name: "Veg Noodles",
-    price: "₹7",
+    price: "₹90",
     description: "Noodles stir-fried with fresh vegetables.",
     category: "Chinese",
-    image: "src/assets/f3.png",
+    image: f3,
   },
   {
     _id: "4",
     name: "Veg Spring Rolls",
-    price: "₹6",
+    price: "₹90",
     description: "Crispy spring rolls stuffed with vegetables.",
     category: "Chinese",
-    image: "src/assets/f4.png",
+    image: f4,
   },
   {
     _id: "5",
     name: "Chicken Kathi Roll",
-    price: "₹12",
+    price: "₹110",
     description: "Flavorful chicken wrapped in a soft roll with spices.",
     category: "Chinese",
-    image: "src/assets/f5.png",
+    image: f5,
   },
   {
     _id: "6",
     name: "Chicken Fried Rice",
-    price: "₹10",
+    price: "₹180",
     description: "Fried rice with chicken and vegetables.",
     category: "Chinese",
-    image: "src/assets/f6.png",
+    image: f6,
   },
   {
     _id: "7",
     name: "Chicken Noodles",
-    price: "₹9",
+    price: "₹120",
     description: "Stir-fried noodles with chicken and vegetables.",
     category: "Chinese",
-    image: "src/assets/f7.png",
+    image: f7,
   },
   {
     _id: "8",
     name: "French Fries",
-    price: "₹5",
+    price: "₹80",
     description: "Crispy and golden French fries.",
     category: "Chinese",
-    image: "src/assets/f8.png",
+    image: f8,
   },
   {
     _id: "9",
     name: "Chilli Chicken",
-    price: "₹11",
+    price: "₹290",
     description: "Spicy and tangy chicken dish.",
     category: "Chinese",
-    image: "src/assets/f9.png",
+    image: f9,
   },
   {
     _id: "10",
     name: "Chicken Momos",
-    price: "₹8",
+    price: "₹130",
     description: "Steamed dumplings filled with chicken.",
     category: "Chinese",
-    image: "src/assets/f10.png",
+    image: f10,
   },
   {
     _id: "11",
     name: "Chicken Lollipop One Pcs",
-    price: "₹2",
+    price: "₹70",
     description: "One piece of spicy chicken lollipop.",
     category: "Chinese",
-    image: "src/assets/f11.png",
+    image: f11,
   },
   {
     _id: "12",
     name: "Egg Fried Rice",
-    price: "₹7",
+    price: "₹170",
     description: "Fried rice with eggs and vegetables.",
     category: "Chinese",
-    image: "src/assets/f12.png",
+    image: f12,
   },
   {
     _id: "13",
     name: "Chicken 65",
-    price: "₹10",
+    price: "₹290",
     description: "Spicy, deep-fried chicken dish.",
     category: "Chinese",
-    image: "src/assets/f13.png",
+    image: f13,
   },
   {
     _id: "14",
     name: "Chilli Paneer",
-    price: "₹9",
+    price: "₹250",
     description: "Spicy and tangy paneer dish.",
     category: "Chinese",
-    image: "src/assets/f14.png",
+    image: f14,
   },
   {
     _id: "15",
     name: "Paneer Momo",
-    price: "₹8",
+    price: "₹120",
     description: "Steamed dumplings filled with paneer.",
     category: "Chinese",
-    image: "src/assets/f15.png",
+    image: f15,
   },
   {
     _id: "16",
     name: "Paneer Fried Rice",
-    price: "₹10",
+    price: "₹150",
     description: "Fried rice with paneer and vegetables.",
     category: "Chinese",
-    image: "src/assets/f16.png",
+    image: f16,
   },
   {
     _id: "17",
     name: "Veg Cheese Burger",
-    price: "₹15",
+    price: "₹90",
     description:
       "A delicious veggie patty topped with cheese, lettuce, and tomato in a soft bun.",
     category: "Burger's",
-    image: "src/assets/f1.png",
+    image: f17,
   },
   {
     _id: "18",
     name: "Chicken Burger",
-    price: "₹20",
+    price: "₹110",
     description:
       "A juicy chicken patty with lettuce, tomato, and mayo in a fresh bun.",
     category: "Burger's",
-    image: "src/assets/f1.png",
+    image: f18,
   },
   {
     _id: "19",
     name: "Plain Naan (1pcs)",
-    price: "₹10",
+    price: "₹35",
     description: "A soft and fluffy plain naan.",
     category: "Bread's",
+    image: f19,
   },
   {
     _id: "20",
     name: "Butter Naan (1pcs)",
-    price: "₹15",
+    price: "₹45",
     description: "A soft naan brushed with butter.",
     category: "Bread's",
+    image: f20,
   },
   {
     _id: "21",
     name: "Tandoori Kulcha (1pcs)",
-    price: "₹20",
+    price: "₹55",
     description: "A crispy and flavorful tandoori kulcha.",
     category: "Bread's",
+    image: f21,
   },
   {
     _id: "22",
     name: "Butter Tandoor Roti (1pcs)",
-    price: "₹12",
+    price: "₹20",
     description: "A traditional tandoori roti brushed with butter.",
     category: "Bread's",
+    image: f22,
   },
   {
     _id: "23",
     name: "Lacchha Parantha (1pcs)",
-    price: "₹15",
+    price: "₹55",
     description: "A multi-layered, flaky lacchha parantha.",
     category: "Bread's",
+    image: f23,
   },
   {
     _id: "24",
     name: "Garlic Naan (1pc)",
-    price: "₹18",
+    price: "₹50",
     description: "A soft naan infused with garlic flavor.",
     category: "Bread's",
+    image: f24,
   },
   {
     _id: "25",
     name: "Plain Tandoori Roti (1pc)",
-    price: "₹10",
+    price: "₹15",
     description: "A traditional plain tandoori roti.",
     category: "Bread's",
+    image: f25,
   },
   {
     _id: "26",
     name: "Zeera Rice",
-    price: "₹30",
+    price: "₹90",
     description: "Aromatic basmati rice cooked with cumin seeds.",
     category: "Dal/Chawal/Curry",
+    image: f26,
   },
   {
     _id: "27",
     name: "Curry Chawal",
-    price: "₹40",
+    price: "₹70",
     description: "Rice served with flavorful curry.",
     category: "Dal/Chawal/Curry",
+    image: f27,
   },
   {
     _id: "28",
     name: "Dal Fry",
-    price: "₹50",
+    price: "₹99",
     description: "Lentils cooked with spices and tempered with ghee.",
     category: "Dal/Chawal/Curry",
+    image: f28,
   },
   {
     _id: "29",
     name: "Rajma Chawal",
-    price: "₹60",
+    price: "₹80",
     description: "Kidney beans curry served with steamed rice.",
     category: "Dal/Chawal/Curry",
+    image: f29,
   },
   {
     _id: "30",
     name: "Dal Makhani",
-    price: "₹70",
+    price: "₹180",
     description: "Creamy black lentils cooked with butter and cream.",
     category: "Dal/Chawal/Curry",
+    image: f30,
   },
   {
     _id: "31",
     name: "Shahi Paneer",
-    price: "₹80",
+    price: "₹420",
     description:
       "Creamy paneer curry made with a rich blend of spices and cream.",
     category: "Veg",
+    image: f31,
   },
   {
     _id: "32",
     name: "Paneer Do Pyaaza",
-    price: "₹90",
+    price: "₹400",
     description: "Paneer cooked with onions in a rich and flavorful gravy.",
     category: "Veg",
+    image: f32,
   },
   {
     _id: "33",
     name: "Paneer Lababdar",
-    price: "₹85",
+    price: "₹440",
     description:
       "Paneer cooked in a creamy tomato-based gravy with aromatic spices.",
     category: "Veg",
+    image: f33,
   },
   {
     _id: "34",
     name: "Matar Paneer",
-    price: "₹75",
+    price: "₹380",
     description:
       "Paneer and peas cooked in a rich and creamy tomato-based gravy.",
     category: "Veg",
+    image: f34,
   },
   {
     _id: "35",
     name: "Paneer Butter Masala",
-    price: "₹85",
+    price: "₹440",
     description:
       "Paneer cooked in a rich and creamy tomato-based gravy with butter.",
     category: "Veg",
+    image: f35,
   },
   {
     _id: "36",
     name: "Paneer Korma",
-    price: "₹90",
+    price: "₹450",
     description: "Paneer cooked in a flavorful and aromatic creamy curry.",
     category: "Veg",
+    image: f36,
   },
   {
     _id: "37",
     name: "Kali Mirch Paneer",
-    price: "₹95",
+    price: "₹450",
     description: "Paneer cooked in a rich and spicy black pepper gravy.",
     category: "Veg",
+    image: f37,
   },
   {
     _id: "38",
-    name: "Kadhai Paneer",
-    price: "₹85",
-    description:
-      "Paneer cooked with bell peppers and onions in a spicy tomato-based gravy.",
+    name: "Paneer Kofta",
+    price: "₹400",
+    description: "Paneer balls cooked in a rich and creamy gravy.",
     category: "Veg",
+    image: f38,
   },
   {
     _id: "39",
-    name: "Shahi Paneer",
-    price: "₹80",
+    name: "Paneer Pasanda",
+    price: "₹400",
     description:
-      "Creamy paneer curry made with a rich blend of spices and cream.",
+      "Paneer cooked in a rich and creamy gravy with almonds and cashews.",
     category: "Veg",
+    image: f39,
   },
   {
     _id: "40",
-    name: "Chicken Lababdar",
-    price: "₹120",
+    name: "Paneer Tikka Masala",
+    price: "₹280",
     description:
-      "Tender chicken cooked in a creamy and flavorful tomato-based gravy.",
-    category: "Non-Veg",
+      "Paneer marinated in spices and cooked in a rich and creamy tomato-based gravy.",
+    category: "Veg",
+    image: f40,
   },
   {
     _id: "41",
     name: "Butter Chicken",
-    price: "₹130",
+    price: "₹550",
     description:
       "Tender chicken cooked in a creamy tomato-based gravy with butter.",
     category: "Non-Veg",
+    image: f41,
   },
   {
     _id: "42",
     name: "Chicken Changezi",
-    price: "₹140",
+    price: "₹520",
     description:
       "Chicken cooked in a rich and spicy gravy with aromatic spices.",
     category: "Non-Veg",
+    image: f42,
   },
   {
     _id: "43",
     name: "Kadhai Chicken",
-    price: "₹110",
+    price: "₹480",
     description:
       "Chicken cooked with bell peppers and onions in a spicy tomato-based gravy.",
     category: "Non-Veg",
+    image: f43,
   },
   {
     _id: "44",
     name: "Kali Mirch Chicken",
-    price: "₹125",
+    price: "₹580",
     description: "Chicken cooked in a rich and spicy black pepper gravy.",
     category: "Non-Veg",
+    image: f44,
   },
   {
     _id: "45",
     name: "Chicken Do Pyaza",
-    price: "₹115",
+    price: "₹540",
     description: "Chicken cooked with onions in a rich and flavorful gravy.",
     category: "Non-Veg",
+    image: f45,
   },
   {
     _id: "46",
     name: "Plain Dosa",
-    price: "₹40",
+    price: "₹90",
     description:
       "Thin, crispy crepe made from fermented rice and lentil batter.",
     category: "South Indian",
+    image: f46,
   },
   {
     _id: "47",
     name: "Paneer Dosa",
-    price: "₹60",
+    price: "₹140",
     description: "Dosa filled with spiced paneer (Indian cottage cheese).",
     category: "South Indian",
+    image: f47,
   },
   {
     _id: "48",
     name: "Masala Dosa",
-    price: "₹50",
+    price: "₹120",
     description: "Crispy dosa filled with a spiced potato filling.",
     category: "South Indian",
+    image: f48,
   },
   {
     _id: "49",
     name: "Rawa Butter Masala Dosa",
-    price: "₹70",
+    price: "₹170",
     description:
       "Dosa made with semolina (rawa) and topped with butter and spicy potato masala.",
     category: "South Indian",
+    image: f49,
   },
   {
     _id: "50",
     name: "Rawa Plain Dosa",
-    price: "₹45",
+    price: "₹110",
     description: "Crispy dosa made with semolina (rawa) batter.",
     category: "South Indian",
+    image: f50,
   },
   {
     _id: "51",
     name: "Afghani Chicken",
-    price: "₹150",
+    price: "₹280",
     description:
       "Tender chicken marinated in yogurt and spices, cooked to perfection in the tandoor.",
-    category: "Tandoor ",
+    category: "Tandoor",
+    image: f51,
   },
   {
     _id: "52",
     name: "Chicken Malai Tikka",
-    price: "₹160",
+    price: "₹320",
     description:
       "Juicy chicken pieces marinated in a creamy mixture of yogurt and spices, grilled to perfection.",
-    category: "Tandoor ",
+    category: "Tandoor",
+    image: f52,
   },
   {
     _id: "53",
     name: "Chicken Tikka",
-    price: "₹140",
+    price: "₹300",
     description:
       "Boneless chicken pieces marinated in a flavorful blend of spices and grilled to perfection.",
-    category: "Tandoor ",
+    category: "Tandoor",
+    image: f53,
   },
   {
     _id: "54",
     name: "Tandoori Chicken",
-    price: "₹130",
+    price: "₹480",
     description:
       "Classic Indian chicken dish marinated in yogurt and spices, cooked to perfection in the tandoor.",
-    category: "Tandoor ",
+    category: "Tandoor",
+    image: f54,
   },
   {
     _id: "55",
     name: "Paneer Malai Tikka",
-    price: "₹120",
+    price: "₹280",
     description:
       "Soft and creamy paneer cubes marinated in a rich blend of cream, yogurt, and spices, grilled to perfection.",
-    category: "Tandoor ",
+    category: "Tandoor",
+    image: f55,
   },
   {
     _id: "56",
     name: "Paneer Tikka",
-    price: "₹110",
+    price: "₹250",
     description:
       "Tender paneer cubes marinated in a flavorful blend of spices and grilled to perfection.",
-    category: "Tandoor ",
+    category: "Tandoor",
+    image: f56,
   },
 ];
